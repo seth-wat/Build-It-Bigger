1316aa1999aaa5789e99f086785fd7285f30fdaa
Seth Watkins <dev.seth.wat@gmail.com>
2017-10-30 10:35:04 -0500

    Renamed modules because I didn't camel case them, deleted an unused directory.

---
+++ androidJokeDisplay/src/main/java/wat/seth/dev/androidjokedisplay/JokeDisplayActivity.java
@@ -0,0 +1,21 @@
+package wat.seth.dev.androidjokedisplay;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeDisplayActivity extends AppCompatActivity {
+    public static final String JOKE_EXTRA = "joke_extra";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondary_main);
+        TextView jokeTextView = (TextView) findViewById(R.id.joke);
+        Intent intent = getIntent();
+        if (getIntent() != null) {
+            jokeTextView.setText(intent.getStringExtra(JOKE_EXTRA));
+        }
+    }
+}
